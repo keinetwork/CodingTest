b61a8860ff48254f38a7102b77b5fd44e9a1c95a
keinetwork <102347379+keinetwork@users.noreply.github.com>
2022-04-30 22:04:04 +0900

    [Bronze III] Title: 별 찍기 - 2, Time: 156 ms, Memory: 15292 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(bf.readLine());
+
+		for (int i = 1; i <= A; i++) {
+			String s="";
+			for(int j=A; j>i; j--) {
+				s +=" ";
+			}
+			for(int k=0; k<i; k++) {
+				s +="*";
+			}
+			bw.write(s + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
