8568fdff21a918d505aa26a7a75091212d8a262e
keinetwork <102347379+keinetwork@users.noreply.github.com>
2022-04-30 21:18:53 +0900

    [Bronze III] Title: 기찍 N, Time: 296 ms, Memory: 21144 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int A = Integer.parseInt(bf.readLine());
+		
+		for (int i = A; i >= 1; i--) {			
+			bw.write(i + "\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
