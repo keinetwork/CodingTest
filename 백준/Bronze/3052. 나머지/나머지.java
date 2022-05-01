fb3204ba7c0a1fece0dde0790ae28fafa32d95b3
keinetwork <102347379+keinetwork@users.noreply.github.com>
2022-05-01 12:06:13 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int A = 0;
 		int B = 42;
@@ -18,9 +15,6 @@ public class Main {
 			A = Integer.parseInt(bf.readLine());
 			st.add(A % B);
 		}
-		bw.write(st.size() + "\n");
-
-		bw.flush();
-		bw.close();
+		System.out.println(st.size());
 	}
 }
