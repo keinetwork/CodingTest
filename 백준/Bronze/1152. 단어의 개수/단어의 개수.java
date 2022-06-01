b12878121aef7f2d1a0b5815ec20583878e47bea
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-01 20:50:36 +0900

    [Bronze II] Title: 단어의 개수, Time: 112 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        int[] c = {0,0};
+        int index =0;
+        int result = 0;
+        c[index] = System.in.read();;
+        while (true) {
+            index = (index+1) % 2;
+            c[index] = System.in.read();
+            if(c[index] < 32) break;
+            if (c[index] == 32) {
+                result++;
+            }
+        }
+        if (c[0] != 32 && c[1] != 32) {
+            result++;
+        }
+        System.out.print(result);
+    }
+}
