26851c3847acef3902adc3159314f647b8a109e6
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-01 23:52:34 +0900

    [Bronze II] Title: 다이얼, Time: 76 ms, Memory: 11428 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,20 +1,11 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[] str = br.readLine().toCharArray();
-
-        int[] dial = {3, 3, 3, 4, 4, 4,
-                5, 5, 5, 6, 6, 6, 7, 7, 7,
-                8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10};
-        int tTime = 0;
-        for (int i = 0; i < str.length; i++) {
-            int a = str[i] - 'A';
-            tTime += dial[a];
+        int n, s = 0;
+        while ((n = System.in.read() - 59) > -1) {
+            s += (n - n / 24 - n / 31) / 3 + 1;
         }
-        System.out.println(tTime);
+        System.out.print(s);
     }
 }
