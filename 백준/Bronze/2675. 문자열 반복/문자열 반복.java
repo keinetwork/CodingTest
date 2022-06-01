77c037a32f157016815702cb92879840103f9634
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-01 16:17:44 +0900

    [Bronze II] Title: 문자열 반복, Time: 72 ms, Memory: 11324 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,42 +1,25 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 
-    private static int T;
-    private static int repeat;
-    private static char[] chArray, resultArray;
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        T = Integer.parseInt(br.readLine());    //  2
-
-        for(int t = 1 ; t <= T ; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            repeat = Integer.parseInt(st.nextToken());  //  3
-            chArray = st.nextToken().toCharArray(); // abc
-
-            resultArray = new char[repeat*chArray.length+1];
-            setData();
-            resultArray[repeat*chArray.length] ='\n';
-            bw.write(resultArray);
-        }
-        bw.flush();
-        bw.close();
-    }
-
-    private static void setData() {
-        int index = 0;
-        for(int i = 0 ; i < chArray.length ; i++) {
-            for(int j = 0 ; j < repeat ; j++) {
-                resultArray[index++] = chArray[i];
+        StringBuilder sb = new StringBuilder();
+        int testCase = Integer.parseInt(br.readLine()); //  2
+        for (int i = 0; i < testCase; i++) {
+            String[] input = br.readLine().split(" ");  // 3 abc
+            int repeat = Integer.parseInt(input[0]);    //  3
+            char[] chars = input[1].toCharArray();      // abc
+            for (int j = 0; j < chars.length; j++) {
+                for (int k = 0; k < repeat; k++) {
+                    sb.append(chars[j]);
+                }
             }
+            sb.append("\n");
         }
+        System.out.print(sb);
     }
 }
