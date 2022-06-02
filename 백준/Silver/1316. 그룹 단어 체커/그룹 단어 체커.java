0d707a8f0f87396a10d3763414705a2785c66738
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-02 15:17:50 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,30 +1,31 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int result = 0;
-
-        int[] alpabet = new int[26];
-        for (int i = 0; i < N; i++, Arrays.fill(alpabet, 0)) {
-            boolean isGroup = true;
-            int prev = -1;
-            for (int cur = br.read(); cur > 10; cur = br.read()) {
-                cur -= 97;
-                if (cur != prev) {
-                    if (alpabet[cur] != 0) {
-                        isGroup = false;
-                    } else {
-                        alpabet[cur]++;
-                        prev = cur;
-                    }
-                }
-            }
-            if (isGroup) result++;
+        for (int i = 0; i < N; i++) {
+            char[] word = br.readLine().toCharArray();
+            if (checkGroup(word))
+                result++;
         }
         System.out.println(result);
+    }
+
+    private static boolean checkGroup(char[] word) {
+        boolean[] alphabet = new boolean[26];
+
+        char pre = 0;
+        for (char w : word) {
+            if (pre != w && alphabet[w - 'a'])
+                return false;
+            pre = w;
+            alphabet[w - 'a'] = true;
+        }
+
+        return true;
     }
 }
