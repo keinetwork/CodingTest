ea14fe0eda35aca3aa40cd3f51b3208ea87ee7e3
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-02 14:58:43 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 80 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        int[] alpabet = new int[26];
+        for (int i = 0; i < N; i++, Arrays.fill(alpabet, 0)) {
+            boolean isGroup = true;
+            int prev = -1;
+            for (int cur = br.read(); cur > 10; cur = br.read()) {
+                cur -= 97;
+                if (cur != prev) {
+                    if (alpabet[cur] != 0) {
+                        isGroup = false;
+                    } else {
+                        alpabet[cur]++;
+                        prev = cur;
+                    }
+                }
+            }
+            if (isGroup) result++;
+        }
+        System.out.println(result);
+    }
+}
