af0822da936b42b3f84d8883439c9d2014068704
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-05 16:50:07 +0900

    [Bronze III] Title: ACM 호텔, Time: 96 ms, Memory: 11804 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -5,15 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
+        int C = Integer.parseInt(br.readLine());
+        for (int i = 0; i < C; i++) {
             String[] input = br.readLine().split(" ");
-            String room = assign(Integer.parseInt(input[0]), Integer.parseInt(input[1]), Integer.parseInt(input[2]));
-            System.out.println(room);
+            int H = Integer.parseInt(input[0]);
+            int W = Integer.parseInt(input[1]);
+            int N = Integer.parseInt(input[2]);
+            System.out.println(assign(H, W, N));
         }
     }
 
     public static String assign(int H, int W, int N) {
-        return String.format("%d%02d", ((N - 1) % H) + 1, (N - 1) / H + 1);
+        int f = ((N - 1) % H) + 1;
+        int n = (N - 1) / H + 1;
+        return Integer.toString(f * 100 + n);
     }
 }
