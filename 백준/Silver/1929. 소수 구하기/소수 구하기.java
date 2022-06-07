98f568e8f6b5b13b85d350735ca9a9a4f1d9e615
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-07 16:47:27 +0900

    [Silver III] Title: 소수 구하기, Time: 172 ms, Memory: 21128 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,38 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
-class Main {
-    public static void main(String args[]) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] str = br.readLine().split(" ");
-        int M = Integer.parseInt(str[0]);
-        int N = Integer.parseInt(str[1]);
-        int[] list = new int[N];
-        int tail = 0;
-        for (int i = 2; i <= N; i++) {
-            boolean check = true;
-            double sqrt = Math.sqrt(i);
-
-            for (int j = 0; j < list.length; j++) {
-                if (list[j] == 0 || list[j] > sqrt) {
-                    break;
-                }
-                if (i % list[j] == 0) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                list[tail++] = i;
-                if (i >= M) {
-                    bw.write(Integer.toString(i) + "\n");
-                }
-            }
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String nm[] = input.readLine().split(" ");       
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        
+        boolean prime[] = new boolean[m+1];
+        
+        for(int i=2; i<=m; i++) {
+        	if(prime[i]) continue;
+        	
+        	if(i >= n) sb.append(i+"\n");
+        	
+        	for(int j=i+i; j<=m; j+=i) {
+        		prime[j] = true;
+        	}
         }
-        bw.flush();
-        bw.close();
+        
+        System.out.println(sb);
     }
 }
