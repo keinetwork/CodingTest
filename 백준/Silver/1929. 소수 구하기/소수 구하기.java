ab7ce8eb2c211b513e8531c570f77e4664b4c43d
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-07 16:38:49 +0900

    [Silver III] Title: 소수 구하기, Time: 324 ms, Memory: 22824 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ListIterator;
 
 class Main {
     public static void main(String args[]) throws Exception {
@@ -13,21 +10,23 @@ class Main {
         String[] str = br.readLine().split(" ");
         int M = Integer.parseInt(str[0]);
         int N = Integer.parseInt(str[1]);
-        List list = new ArrayList<Integer>();
+        int[] list = new int[N];
+        int tail = 0;
         for (int i = 2; i <= N; i++) {
             boolean check = true;
             double sqrt = Math.sqrt(i);
-            ListIterator it = list.listIterator();
-            while (it.hasNext()) {
-                int sosu = (int) it.next();
-                if (i % sosu == 0) {
+
+            for (int j = 0; j < list.length; j++) {
+                if (list[j] == 0 || list[j] > sqrt) {
+                    break;
+                }
+                if (i % list[j] == 0) {
                     check = false;
                     break;
                 }
-                if (sosu >= sqrt) break;
             }
             if (check) {
-                list.add(i);
+                list[tail++] = i;
                 if (i >= M) {
                     bw.write(Integer.toString(i) + "\n");
                 }
