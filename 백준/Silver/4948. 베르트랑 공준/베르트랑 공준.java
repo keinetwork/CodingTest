3e69dcb2dcc9f1344e1beff79efef507f2870ac6
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-10 16:27:18 +0900

    [Silver II] Title: 베르트랑 공준, Time: 96 ms, Memory: 12264 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -2,43 +2,61 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
+    static final int MAX = 123456 * 2;
+    static boolean[] isPrime = new boolean[MAX + 1];
+    static int[] prime = new int[21790];
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        List<Integer> list = new ArrayList<>();
-        int n = 0;
-        int max = 0;
-        while ((n = Integer.parseInt(br.readLine())) != 0) {
-            list.add(n);
-            if (n > max) max = n;
-        }
-        max *= 2;
-        boolean[] prime = new boolean[max + 1];
-        prime[0] = true;
-        prime[1] = true;
-        double sqrt = Math.sqrt(max);
-        for (int i = 2; i <= sqrt; i++) {
-            for (int j = i + i; j <= max; j += i) {
-                if (prime[j] == false)
-                    prime[j] = true;
+        int size = setPrime();
+        int n = Integer.parseInt(br.readLine());
+        while (n != 0) {
+            int idx1 = binarySearch(n, size);
+            int idx2 = binarySearch(n * 2, size);
+            if (prime[idx2] == n * 2) {
+                idx2++;
+            } else if (prime[idx1] == n) {
+                idx1++;
             }
-        }
-
-        for (Integer l : list) {
-            n = l * 2;
-            int count = 0;
-            for (int i = l + 1; i <= n; i++) {
-                if (prime[i] == false) count++;
-            }
-            bw.write(Integer.toString(count) + "\n");
+            bw.write(Integer.toString(idx2 - idx1) + "\n");
+            n = Integer.parseInt(br.readLine());
         }
         br.close();
         bw.flush();
         bw.close();
+    }
+
+    public static int binarySearch(int num, int size) {
+        int start = 1;
+        int end = size - 1;
+        while (start <= end) {
+            int center = (start + end) / 2;
+            if (prime[center] >= num) {
+                end = center - 1;
+            } else {
+                start = center + 1;
+            }
+        }
+        return start;
+    }
+
+    public static int setPrime() {
+        int idx = 1;
+        isPrime[0] = true;
+        isPrime[1] = true;
+        double sqrt = Math.sqrt(MAX);
+
+        for (int i = 2; i < MAX; i++) {
+            if (isPrime[i]) continue;
+            prime[idx++] = i;
+            if (i > sqrt) continue;
+            for (int j = i * i; j <= MAX; j += i) {
+                isPrime[j] = true;
+            }
+        }
+        return idx;
     }
 }
