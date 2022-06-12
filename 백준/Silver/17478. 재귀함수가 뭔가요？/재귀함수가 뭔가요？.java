e57ac038ddb49d6eed4e569ecfb5ef54df1fbe44
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-12 12:41:05 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 80 ms, Memory: 11780 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,44 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    static String[] str = {"어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n",
-            "\"재귀함수가 뭔가요?\"\n",
-            "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n",
-            "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n",
-            "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n",
-            "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n",
-            "라고 답변하였지.\n"
-    };
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static int n;
 
-    public static void chatbot(int a, int b, StringBuilder sb) {
-        String prefix = "";
-        for (int i = 0; i < a; i++) {
-            prefix += "____";
+    public static void chatbot(int num) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            sb.append("____");
         }
-        if (a == 0) {
-            sb.append(str[0]);
+        bw.write(sb + "\"재귀함수가 뭔가요?\"\n");
+        if (num == n) {
+            bw.write(sb + "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
+        } else {
+            bw.write(sb + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+            bw.write(sb + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+            bw.write(sb + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+            chatbot(num + 1);
         }
-        if (a < b) {
-            sb.append(prefix).append(str[1]);
-            sb.append(prefix).append(str[2]);
-            sb.append(prefix).append(str[3]);
-            sb.append(prefix).append(str[4]);
-            chatbot(a + 1, b, sb);
-        }
-
-        if (a == b && a > 0) {  // 마지막
-            sb.append(prefix).append(str[1]);
-            sb.append(prefix).append(str[5]);
-        }
-        sb.append(prefix).append(str[6]);        //답변
+        bw.write(sb + "라고 답변하였지.\n");
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        chatbot(0, N, sb);
-        System.out.println(sb.toString());
+        n = Integer.parseInt(br.readLine());
+        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
+        chatbot(0);
+        bw.flush();
     }
 }
