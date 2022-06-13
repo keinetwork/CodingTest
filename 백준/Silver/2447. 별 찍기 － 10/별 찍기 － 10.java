941edf9288023df2b3222d0cea39b4ad315e7ae4
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-13 15:02:50 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 236 ms, Memory: 22288 KB -BaekjoonHub

--- 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -20,18 +20,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        char[] star = new char[N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (isBlank(i, j, N)) {
-                    star[j] = ' ';
+        char[][] star = new char[N][N];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i % 3 == 1 && j % 3 == 1) {
+                    star[i][j] = ' ';
                 } else {
-                    star[j] = '*';
+                    star[i][j] = '*';
                 }
             }
-            bw.write(star);
+        }
+        int[] ep = {3, 6};
+        int[] cp = {3, 9};
+        while (cp[1] <= N) {
+            for (int i = 0; i < cp[1]; i++) {
+                for (int j = 0; j < cp[1]; j++) {
+                    if (star[i][j] != 0) continue;
+                    if (i >= ep[0] && i < ep[1] && j >= ep[0] && j < ep[1]) {
+                        star[i][j] = ' ';
+                        continue;
+                    }
+                    star[i][j] = star[i % cp[0]][j % cp[0]];
+                }
+            }
+            ep[0] *= 3;
+            ep[1] = ep[0] * 2;
+            cp[0] *= 3;
+            cp[1] *= 3;
+        }
+        for (int i = 0; i < cp[0]; i++) {
+            bw.write(star[i]);
             bw.newLine();
         }
+
         br.close();
         bw.flush();
         bw.close();
