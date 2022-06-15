cb637861a509c4e35009782906b1052f226ed39f
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-15 11:46:30 +0900

    [Bronze II] Title: 블랙잭, Time: 80 ms, Memory: 11616 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,24 +1,38 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer token = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(token.nextToken());
+        int M = Integer.parseInt(token.nextToken());
         int[] card = new int[N];
+        token = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++) {
-            card[i] = scanner.nextInt();
+            card[i] = Integer.parseInt(token.nextToken());
         }
+        int answer = black(card, N, M);
+        System.out.println(answer);
+    }
+
+    private static int black(int[] arr, int N, int M) {
         int max = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
+        for (int i = 0; i < N - 2; i++) {
+            if (arr[i] > M) continue;
+            for (int j = i + 1; j < N - 1; j++) {
+                if (arr[i] + arr[j] > M) continue;
                 for (int k = j + 1; k < N; k++) {
-                    int num = card[i] + card[j] + card[k];
+                    int num = arr[i] + arr[j] + arr[k];
+                    if (M == max) {
+                        return max;
+                    }
                     if (num <= M && num > max) max = num;
                 }
             }
         }
-        System.out.println(max);
+        return max;
     }
 }
