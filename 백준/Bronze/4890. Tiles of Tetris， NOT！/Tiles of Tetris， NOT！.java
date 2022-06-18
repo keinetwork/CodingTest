bbfd501b926a78d00ec2343950a5caed949efab4
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-18 14:59:03 +0900

    [Bronze II] Title: Tiles of Tetris, NOT!, Time: 116 ms, Memory: 12988 KB -BaekjoonHub

---
+++ 백준/Bronze/4890. Tiles of Tetris， NOT！/Tiles of Tetris， NOT！.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int W = scanner.nextInt();
+        int H = scanner.nextInt();
+        while (W != 0 && H != 0) {
+            int gcd = GCD(W, H);
+            long lcm = (long) W / gcd * H;
+            long ans = (lcm / W) * (lcm / H);
+            System.out.println(ans);
+            W = scanner.nextInt();
+            H = scanner.nextInt();
+        }
+    }
+
+    // greatest common divisor
+    static int GCD(int a, int b) {
+        if (b == 0) return a;
+        return GCD(b, a % b);
+    }
+
+    // least common multiple (a*b/GCD(a,b)) oerverflow
+    static int LCM(int a, int b) {
+        return (a * b) / GCD(a, b);
+    }
+}
