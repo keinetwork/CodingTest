e6595beb3317e543e8c614fdd16732f89f2f31ad
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-21 16:34:25 +0900

    [Silver V] Title: 박 터뜨리기, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.java
+++ 백준/Silver/19939. 박 터뜨리기/박 터뜨리기.java
@@ -1,40 +1,28 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int K = scanner.nextInt();
-        Main main = new Main();
-        int ans = main.result(N, K);
-        System.out.println(ans);
-    }
-
-    public int result(int N, int K) {
-        int maxP = -1;
-        int[] ans = new int[K];
-        for (int i = 0; i < K; i++, maxP++) {
-            ans[i] = i + 1;
-            N -= i + 1;
-        }
-        if (N < 0) return -1;
-        while (N > 0) {
-            for (int i = 0; i < K; i++) {
-                if (N == 0) return ans[maxP] - ans[(maxP + 1) % K];
-                if (N < K) {
-                    for (int j = 0; j < K; j++) {
-                        int index = (maxP + 1 + j) % K;
-                        if (ans[index] + N > ans[maxP]) {
-                            return ans[index] + N - ans[(maxP + 1) % K];
-                        }
-                    }
-                }
-                ans[i] += K;
-                N -= K;
-                maxP = ++maxP % K;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < K + 1; i++) {
+            N -= i;
+            if (N < 0) {
+                break;
             }
         }
-        return ans[maxP] - ans[(maxP + 1) % K];
+        if (N >= 0) {
+            if (N % K > 0) {
+                System.out.println(K);
+            } else {
+                System.out.println(K - 1);
+            }
+        } else {
+            System.out.println(-1);
+        }
     }
 }
