7f4343c420f4f374c397ced98d63d3724ce55313
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-21 09:43:22 +0900

    [Silver V] Title: 덩치, Time: 76 ms, Memory: 11656 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,11 +1,10 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         int[][] xy = new int[N][2];
         for (int i = 0; i < N; i++) {
@@ -21,8 +20,8 @@ public class Main {
                     ans++;
                 }
             }
-            System.out.print(ans + " ");
+            bw.write(ans + " ");
         }
+        bw.flush();
     }
-
 }
