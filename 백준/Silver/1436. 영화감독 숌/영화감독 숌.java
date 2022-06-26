92c00bdc0584f6980f976aec9df8d5e3c5204cf5
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-26 23:39:21 +0900

    [Silver V] Title: 영화감독 숌, Time: 80 ms, Memory: 11784 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,34 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(br.readLine());
         System.out.println(find(N));
     }
 
-    public static int find(int N) {
-        int ans = 0; // 영화 제목
-        int cnt = 0; // 현재 몇번쨰 종말의 수인지
-
-        int temp = 0;
-        while (cnt != N) {
-            ans++;
-            temp = ans;
-            // 수에 6이 적어도 3개이상 들어가는지 판별
-            while (temp != 0) {
-                if (temp % 1000 == 666) { // 종말의 숫자라면
-                    cnt++;
-                    break;
-                } else {
-                    temp /= 10;// 일의 자리수 삭제
+    public static String find(int N) {
+        if (N < 1) return "0";
+        if (N == 1) return "666";
+        String res = "";
+        int count = 0;
+        for (int index = 0; ; index++) {
+            int fCount = findCount(index);
+            if (N <= fCount + count) {
+                res = Integer.toString(index);
+                for (int i = (int) Math.log10(fCount); i < 3; i++) {
+                    res += "6";
                 }
+                if (0 < Math.log10(fCount)) {
+                    String format = "%0" + (int) Math.log10(fCount) + "d";
+                    res += String.format(format, N - count - 1);
+                }
+                return res;
+            } else {
+                count += fCount;
             }
         }
-        return ans;
+    }
+
+    public static int findCount(int num) {
+        int result = 1;
+        while (num % 10 == 6) {
+            result *= 10;
+            num /= 10;
+        }
+        return result;
     }
 }
