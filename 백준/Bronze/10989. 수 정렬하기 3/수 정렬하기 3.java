928e3818d3286aaaf7c0f5de7e3bda0705317e75
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-29 15:17:32 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1772 ms, Memory: 296692 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -10,10 +10,10 @@ public class Main {
             list[Integer.parseInt(br.readLine())]++;
         }
         for (int i = 1; i < 10001; i++) {
-            for (int j = 0; j < list[i]; j++) {
-                bw.write(Integer.toString(i));
-                bw.newLine();
-            }
+            if (list[i] != 0)
+                for (int j = 0; j < list[i]; j++) {
+                    bw.write(String.valueOf(i + "\n"));
+                }
         }
         bw.flush();
     }
