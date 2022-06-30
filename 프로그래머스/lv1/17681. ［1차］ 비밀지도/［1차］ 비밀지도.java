95e3edb1a54e0dccb523aec9024a58dcd4b12d6e
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-01 00:25:05 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.05 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,31 +1,39 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i <n ; i++) {
-            answer[i] = "";
-            String block;
+            sb.setLength(0);
             for (int j = 1; j <n ; j++) {
-                block = " ";
+                boolean block=false;
                 int div = (int)Math.pow(2,n-j);
                 if(arr1[i]/div>0){
-                    block="#";
+                    block=true;
                     arr1[i]%=div;
                 }
                 if(arr2[i]/div>0){
-                    block="#";
+                    block=true;
                     arr2[i]%=div;
                 }
-                answer[i] += block;
+                if(block){
+                    sb.append("#");
+                } else{
+                    sb.append(" ");
+                }
             }
-            block = " ";
+            boolean block=false;
             if(arr1[i]%2>0){
-                block="#";
+                block = true;
             }
-            if(arr2[i]%2>0){
-                block="#";
+            if(!block &&arr2[i]%2>0){
+                block=true;
             }
-            answer[i] += block;
+            if(block){
+                sb.append("#");
+            } else{
+                sb.append(" ");
+            }
+            answer[i] = sb.toString();
         }
         return answer;
     }
