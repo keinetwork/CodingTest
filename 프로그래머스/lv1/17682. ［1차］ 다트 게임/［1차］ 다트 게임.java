53f3939d06856d3f366fe52d67cfb0faa01a6412
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-01 02:10:44 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.06 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     char[] dart;
+    int[] score;
     public int getScore(int start, int end){
         int val =0;
         int rate = 1;
@@ -11,26 +12,30 @@ class Solution {
         }
         return val;
     }
+    public int getOpt(int index, int pos){
+        if(dart[pos+1]=='*'){
+            if(index>0) {
+                score[index - 1] *= 2;
+            }
+            score[index] *= 2;
+            return 1;
+        } else if(dart[pos+1]=='#'){
+            score[index] *= -1;
+            return 1;
+        }
+        return 0;
+    }
     public int solution(String dartResult) {
         dart = dartResult.toCharArray();
+        score = new int[3];
         int start = 0;
         int index =0;
-        int[] score = {0,0,0};
         for (int i = start; i <dart.length ; i++) {
             if(dart[i]=='S'){
                 score[index] = getScore(start, i-1);
                 score[index] = (int)Math.pow(score[index], 1);
                 if(i+1<dart.length){
-                    if(dart[i+1]=='*'){
-                        if(index>0) {
-                            score[index - 1] *= 2;
-                        }
-                        score[index] *= 2;
-                        i++;
-                    } else if(dart[i+1]=='#'){
-                        score[index] *= -1;
-                        i++;
-                    }
+                    i+= getOpt(index, i);
                 }
                 index++;
                 start = i+1;
@@ -39,16 +44,7 @@ class Solution {
                 score[index] = getScore(start, i-1);
                 score[index] = (int)Math.pow(score[index], 2);
                 if(i+1<dart.length){
-                    if(dart[i+1]=='*'){
-                        if(index>0) {
-                            score[index - 1] *= 2;
-                        }
-                        score[index] *= 2;
-                        i++;
-                    } else if(dart[i+1]=='#'){
-                        score[index] *= -1;
-                        i++;
-                    }
+                    i+= getOpt(index, i);
                 }
                 start = i+1;
                 index++;
@@ -57,16 +53,7 @@ class Solution {
                 score[index] = getScore(start, i-1);
                 score[index] = (int)Math.pow(score[index], 3);
                 if(i+1<dart.length){
-                    if(dart[i+1]=='*'){
-                        if(index>0) {
-                            score[index - 1] *= 2;
-                        }
-                        score[index] *= 2;
-                        i++;
-                    } else if(dart[i+1]=='#'){
-                        score[index] *= -1;
-                        i++;
-                    }
+                    i+= getOpt(index, i);
                 }
                 start = i+1;
                 index++;
