5982e962f9008ec992a3ef65daf6d84806c8d6d5
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-06-30 13:15:00 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.24 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -0,0 +1,45 @@
+class Solution {
+    int[][] arr = {{3,1},
+                   {0,0},{0,1},{0,2},
+                    {1,0},{1,1},{1,2},
+                    {2,0},{2,1},{2,2},
+                    {3,0},{3,2}};
+    public String solution(int[] numbers, String hand) {
+        int[] left ={3,0};
+        int[] right ={3,2};
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<numbers.length; i++){
+            int num = numbers[i];
+            if(arr[num][1]==0) {
+                left = arr[num];
+                sb.append("L");
+            }
+            if(arr[num][1]==2) {
+                right = arr[num];
+                sb.append("R");
+            }
+            if(arr[num][1]==1){
+                int[] nPos = arr[num];
+                int x = Math.abs(nPos[0]-left[0])+Math.abs(nPos[1]-left[1]);
+                int y = Math.abs(nPos[0]-right[0])+Math.abs(nPos[1]-right[1]);
+                if(x==y){
+                    if(hand.equals("right")){
+                        right = arr[num];
+                        sb.append("R");
+                    }
+                    else{
+                        left = arr[num];
+                        sb.append("L");
+                    }
+                } else if (x < y) {
+                    left = arr[num];
+                    sb.append("L");
+                } else {
+                    right = arr[num];
+                    sb.append("R");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
