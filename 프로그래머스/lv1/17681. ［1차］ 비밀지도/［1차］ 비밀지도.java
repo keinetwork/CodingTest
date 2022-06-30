8c081059376993197ea621d35407c81d834f9a28
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-01 00:45:18 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.02 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,39 +1,12 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i <n ; i++) {
-            sb.setLength(0);
-            for (int j = 1; j <n ; j++) {
-                boolean block=false;
-                int div = (int)Math.pow(2,n-j);
-                if(arr1[i]/div>0){
-                    block=true;
-                    arr1[i]%=div;
-                }
-                if(arr2[i]/div>0){
-                    block=true;
-                    arr2[i]%=div;
-                }
-                if(block){
-                    sb.append("#");
-                } else{
-                    sb.append(" ");
-                }
-            }
-            boolean block=false;
-            if(arr1[i]%2>0){
-                block = true;
-            }
-            if(!block &&arr2[i]%2>0){
-                block=true;
-            }
-            if(block){
-                sb.append("#");
-            } else{
-                sb.append(" ");
-            }
-            answer[i] = sb.toString();
+        for (int i = 0; i < n; i++) {
+            answer[i] = String.format("%" + n + "s",Integer.toBinaryString(arr1[i] | arr2[i]));
+        }
+        for (int i = 0; i < n; i++) {
+            answer[i] = answer[i].replaceAll("1", "#");
+            answer[i] = answer[i].replaceAll("0", " ");
         }
         return answer;
     }
