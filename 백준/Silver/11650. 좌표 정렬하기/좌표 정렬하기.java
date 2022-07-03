f9db580939287bde8f3b4791e66ecb3fec8a5307
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-03 10:26:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1180 ms, Memory: 97076 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -1,30 +1,33 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Arrays;
- 
-public class Main {
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int N = in.nextInt();
-		
-		int[][] arr = new int[N][2];
-		
-		for(int i = 0; i < N; i++) {
-			arr[i][0] = in.nextInt();
-			arr[i][1] = in.nextInt();
-		}
-		
-		Arrays.sort(arr, (e1, e2) -> {
-			if(e1[0] == e2[0]) {
-				return e1[1] - e2[1];
-			} else {
-				return e1[0] - e2[0];
-			}
-		});
-		
-		for(int i = 0; i < N; i++) {
-			System.out.println(arr[i][0] + " " + arr[i][1]);
-		}
-	}
+import java.util.Comparator;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        }
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0])
+                    return o1[1] - o2[1];
+                return o1[0] - o2[0];
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1]);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
 }
