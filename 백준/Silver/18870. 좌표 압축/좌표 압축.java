384433913c68a7b9e0695bc0b794bb91a124ca83
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-03 13:53:04 +0900

    [Silver II] Title: 좌표 압축, Time: 2216 ms, Memory: 352600 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String args[]) throws IOException {
@@ -10,15 +10,15 @@ public class Main {
 
         int T = Integer.parseInt(br.readLine());
         int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Map<Integer, Integer> map = new TreeMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            map.put(arr[i], 0);
-        }
+        int[] sort = Arrays.stream(arr).toArray();
+        Arrays.sort(sort);
+        Map<Integer, Integer> map = new HashMap<>();
         int index = 0;
-        for (Integer i : map.keySet()) {
-            map.put(i, index++);
+        for (int i = 0; i < sort.length; i++) {
+            if (!map.containsKey(sort[i])) {
+                map.put(sort[i], index++);
+            }
         }
-
         for (int i = 0; i < arr.length; i++) {
             bw.write(map.get(arr[i]) + " ");
         }
