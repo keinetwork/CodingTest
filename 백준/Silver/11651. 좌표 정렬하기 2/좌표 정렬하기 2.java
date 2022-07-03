626ca74639556b6c90581e850ab9b2194c5f667e
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-03 11:08:18 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 544 ms, Memory: 45152 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.TreeMap;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        Map<Long, String> map = new TreeMap<>();
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
+            long h1 = Integer.parseInt(st.nextToken());
+            long h2 = Integer.parseInt(st.nextToken()) * 1000000L;
+            map.put(h1 + h2, str);
+        }
+        for (Long i : map.keySet()) {
+            bw.write(map.get(i));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
