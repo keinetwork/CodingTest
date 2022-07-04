b8946cff1b7cd5673eba6198deee37429c020e01
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-04 13:55:43 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.02 ms
+메모리: 76 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,13 +1,9 @@
 class Solution {
     public int solution(int[] numbers) {
-        boolean[] check = new boolean[10];
-        for (int i = 0; i <numbers.length ; i++) {
-            check[numbers[i]] = true;
+        int res = 45;
+        for (int i : numbers) {
+            res -= i;
         }
-        int answer = 0;
-        for (int i = 0; i <check.length ; i++) {
-            if(!check[i]) answer += i;
-        }
-        return answer;
+        return res;
     }
 }
