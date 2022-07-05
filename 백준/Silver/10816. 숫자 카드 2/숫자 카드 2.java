5c6b78352d664209896387480b33d9b72f3194f8
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-05 15:00:57 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 816 ms, Memory: 254520 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,35 +1,23 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int offset = 10_000_000;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] arr = new int[offset * 2 + 1];
         for (int i = 0; i < N; i++) {
-            int card = Integer.parseInt(st.nextToken());
-            Integer val = map.get(card);
-            if (val == null) {
-                map.put(card, 1);
-            } else {
-                map.put(card, val + 1);
-            }
+            arr[Integer.parseInt(st.nextToken()) + offset]++;
         }
         int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
-            int card = Integer.parseInt(st.nextToken());
-            Integer val = map.get(card);
-            if (val == null) {
-                sb.append(0);
-            } else {
-                sb.append(val);
-            }
-            sb.append(" ");
+            sb.append(arr[Integer.parseInt(st.nextToken()) + offset]).append(" ");
         }
         System.out.println(sb);
     }
