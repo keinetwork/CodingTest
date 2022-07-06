dceb6fca60a80c70e8fe577d7a65d5b9b32a4f28
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-06 18:01:49 +0900

    [Bronze III] Title: 택시 기하학, Time: 76 ms, Memory: 11612 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int R = Integer.parseInt(br.readLine());
+        System.out.println(R * R * Math.PI);
+        System.out.println(R * R * 2);
+    }
+}
