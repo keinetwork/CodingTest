e8d3396368d9b00661bad31897271bbd948b507c
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-11 19:03:18 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 11568 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -7,23 +7,21 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder out = new StringBuilder();
+        int[][] dp = new int[30][30];
+        for (int a = 0; a < 30; a++) {
+            for (int b = 0; b <= a; b++)
+                if (b == 0 || a == b) {
+                    dp[a][b] = 1;
+                } else {
+                    dp[a][b] = dp[a - 1][b - 1] + dp[a - 1][b];
+                }
+        }
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             StringTokenizer in = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(in.nextToken());
             int M = Integer.parseInt(in.nextToken());
-            if (N > M / 2) {
-                N = M - N;
-            }
-            long a = 1;
-            long b = 1;
-            for (int j = M - N + 1; j <= M; j++) {
-                a *= j;
-            }
-            for (int j = 2; j <= N; j++) {
-                b *= j;
-            }
-            out.append(a / b).append("\n");
+            out.append(dp[M][N]).append("\n");
         }
         System.out.println(out);
     }
