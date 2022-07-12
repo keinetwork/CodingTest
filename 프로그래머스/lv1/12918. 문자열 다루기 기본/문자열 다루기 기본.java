5c5e4a947869f8a508a38ea1b5a4fc03ccd99131
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-12 17:59:31 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.14 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,9 +2,6 @@ import java.util.regex.Pattern;
 
 class Solution {
     public boolean solution(String s) {
-        if(s.length() == 4 || s.length()==6){
-            return Pattern.matches("^\\d*$", s);    
-        }
-        return false;
+            return Pattern.matches("^\\d{4}|{6}$", s);
     }
 }
