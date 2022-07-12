45b9c11b32d1d2d64d162112390d5d3944c362b1
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-12 18:11:13 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.86 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,11 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public String solution(String[] seoul) {
-        String answer = "Kim";
-        for(int i=0; i<seoul.length;i++){
-            if(seoul[i].equals(answer)){
-                return String.format("김서방은 %d에 있다", i);
-            }
-        }
-        return answer;
+        int x = Arrays.asList(seoul).indexOf("Kim");
+        return String.format("김서방은 %d에 있다", x);
     }
 }
