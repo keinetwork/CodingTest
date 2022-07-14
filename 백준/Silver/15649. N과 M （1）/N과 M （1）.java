83f0fb19d184f248bf64ae072a511e85b6e52252
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-14 13:59:19 +0900

    [Silver III] Title: N과 M (1), Time: 128 ms, Memory: 18284 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,49 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arr;
-    public static boolean[] visit;
-    public static StringBuilder out = new StringBuilder();
+    static int N, M;
+    static boolean[] check;
+    static char[] answer;
+
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(N, M, 0);
-        System.out.println(out);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        check = new boolean[N + 1];
+        answer = new char[2 * M];
+
+        for (int i = 0; i < M - 1; i++)
+            answer[2 * i + 1] = ' ';
+
+        answer[2 * M - 1] = '\n';
+
+        dfs(0);
+
+        System.out.println(sb);
     }
 
-    public static void dfs(int N, int M, int depth) {
-        if (depth == M) {
-            for (int val : arr) {
-                out.append(val).append(' ');
-            }
-            out.append('\n');
+    static void dfs(int m) {
+        if (m == M) {
+            sb.append(answer);
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1);
-                visit[i] = false;
+        for (int i = 1; i <= N; i++) {
+            if (!check[i]) {
+                check[i] = true;
+                answer[2 * m] = (char) (i + '0');
+
+                dfs(m + 1);
+
+                check[i] = false;
             }
         }
     }
