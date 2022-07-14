3baf792a92b28ccc1004b19936db42f394c7eb0f
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-14 14:15:38 +0900

    [Silver III] Title: N과 M (2), Time: 88 ms, Memory: 11460 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,35 +1,41 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int[] arr;
+    public static char[] answer;
     public static boolean[] visit;
     public static StringBuilder out = new StringBuilder();
+    static int N;
+    static int M;
 
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        int M = in.nextInt();
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(N, M, 0, 0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer in = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(in.nextToken());
+        M = Integer.parseInt(in.nextToken());
+        answer = new char[M * 2];
+        visit = new boolean[N + 1];
+        for (int i = 0; i < M - 1; i++)
+            answer[2 * i + 1] = ' ';
+
+        answer[2 * M - 1] = '\n';
+        dfs(0, 1);
         System.out.println(out);
     }
 
-    public static void dfs(int N, int M, int depth, int next) {
+    public static void dfs(int depth, int next) {
         if (depth == M) {
-            for (int val : arr) {
-                out.append(val).append(' ');
-            }
-            out.append('\n');
+            out.append(answer);
             return;
         }
 
-        for (int i = next; i < N; i++) {
+        for (int i = next; i <= N; i++) {
             if (!visit[i]) {
                 visit[i] = true;
-                arr[depth] = i + 1;
-                dfs(N, M, depth + 1, i + 1);
+                answer[2 * depth] = (char) (i + '0');
+                dfs(depth + 1, i + 1);
                 visit[i] = false;
             }
         }
