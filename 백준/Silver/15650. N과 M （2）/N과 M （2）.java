a5cd3be84ab846ae61c2fe565a05f8d48d2264f8
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-14 13:45:11 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static int[] arr;
+    public static boolean[] visit;
+    public static StringBuilder out = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0, 0);
+        System.out.println(out);
+    }
+
+    public static void dfs(int N, int M, int depth, int next) {
+        if (depth == M) {
+            for (int val : arr) {
+                out.append(val).append(' ');
+            }
+            out.append('\n');
+            return;
+        }
+
+        for (int i = next; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1, i + 1);
+                visit[i] = false;
+            }
+        }
+
+    }
+}
