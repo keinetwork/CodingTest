76890d3a2c44965d818caba4507db2135e6d5db1
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-17 21:24:17 +0900

    [Gold IV] Title: N-Queen, Time: 76 ms, Memory: 11480 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,45 +1,10 @@
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int answer = 0;
-    static int[][] queen;
-
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(System.in);
-        N = in.nextInt();
-        queen = new int[N][2];
-        dfs(0);
-        System.out.println(answer);
-    }
-
-    public static boolean check(int depth, int y) {
-        for (int i = 0; i < depth; i++) {
-            // 수평검사
-//            if (queen[i][0] == depth) return false;
-            // 수직 검사
-            if (queen[i][1] == y) return false;
-            // 대각선 검사
-            if ((queen[i][0] - depth) == (queen[i][1] - y)) return false;
-            if ((queen[i][0] - depth) == (y - queen[i][1])) return false;
-
-        }
-        return true;
-    }
-
-    public static void dfs(int depth) {
-        if (depth == N) {
-            answer++;
-            return;
-        }
-        queen[depth][0] = depth;
-        for (int y = 0; y < N; y++) {
-            if (check(depth, y)) {
-                queen[depth][1] = y;
-                dfs(depth + 1);
-            }
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = { 0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596 };
+        System.out.print(arr[n]);
     }
 }
