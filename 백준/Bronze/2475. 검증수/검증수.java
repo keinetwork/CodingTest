6a7fd103034d37fb28aab79ecea5ebab428ece7b
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-22 11:38:01 +0900

    [Bronze V] Title: 검증수, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,11 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int res = 0;
         for (int i = 0; i < 5; i++) {
-            res += Math.pow(in.nextInt(), 2);
+            res += Math.pow(Integer.parseInt(st.nextToken()), 2);
         }
         System.out.println(res % 10);
     }
