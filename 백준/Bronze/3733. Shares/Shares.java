c3dacf0d84a36fe970ce97ce501dbe4b0375da2a
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-29 10:22:17 +0900

    [Bronze V] Title: Shares, Time: 76 ms, Memory: 11308 KB -BaekjoonHub

--- 백준/Bronze/3733. Shares/Shares.java
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -1,13 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        while (sc.hasNextInt()) {
-            int N = sc.nextInt();
-            int S = sc.nextInt();
-            System.out.println(S / (N + 1));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String str;
+        while ((str = br.readLine())!= null ) {
+            StringTokenizer st = new StringTokenizer(str); 
+            int N = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            sb.append(S / (N + 1)).append("\n");
         }
+        System.out.println(sb);
     }
 }
