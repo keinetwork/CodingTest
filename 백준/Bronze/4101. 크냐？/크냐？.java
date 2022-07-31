8754cc0556d80ee5225c0de43bd618ae2fb677a6
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-07-31 09:11:57 +0900

    [Bronze V] Title: 크냐?, Time: 84 ms, Memory: 11652 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            if (A == 0 && B == 0) break;
+            sb.append(A > B ? "Yes" : "No").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
