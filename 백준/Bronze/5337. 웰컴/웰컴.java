aa552c1eeff1af037fe3625d6ba244ff5d8b2034
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-08-02 10:44:11 +0900

    [Bronze V] Title: 웰컴, Time: 72 ms, Memory: 11224 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(sb);
+    }
+}
