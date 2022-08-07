3b7f82e86ec8b2b5129dfc34e1cbc05228a2e5ae
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-08-07 13:14:08 +0900

    [Bronze V] Title: 등록, Time: 76 ms, Memory: 11232 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(167);
+        System.out.println("keinetwork");
+    }
+}
