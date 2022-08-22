52cf73d94a91ce9ab839bffa0f3ff77849fa23fd
김영석 <102347379+keinetwork@users.noreply.github.com>
2022-08-22 22:25:45 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,37 +1,39 @@
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] height = new int[9];
-        int sum = 0;
-        for (int i = 0; i < height.length; i++) {
-            height[i] = Integer.parseInt(br.readLine());
-            sum += height[i];
-        }
-        Arrays.sort(height);
-
-        int fake1 = 0;
-        int fake2 = 0;
-        for (int i = 0; i < height.length - 1; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                if ((sum - height[i] - height[j]) == 100) {
-                    fake1 = i;
-                    fake2 = j;
-                    break;
-                }
-            }
-        }
-
-        for (int i = 0; i < height.length; i++) {
-            if ((i == fake1) || (i == fake2)) {
-                continue;
-            }
-            System.out.println(height[i]);
-        }
-    }
+	
+	static int sum = 0;
+	
+	public static int solution(int[] input) {
+		
+		for(int i=0;i<8;i++) {
+			for(int j=i+1;j<9;j++) {
+				if((sum-input[i]-input[j] == 100)) {
+					input[i] = input[j] = 100;
+					return 0;
+				}
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] input = new int[9];
+		for(int i=0;i<9;i++) {
+			input[i] = Integer.parseInt(br.readLine());
+			sum += input[i];
+		}
+		
+		solution(input);
+		
+		Arrays.sort(input);
+		
+		for(int i=0;i<7;i++) {
+			System.out.println(input[i]);
+		}
+	}
 }
